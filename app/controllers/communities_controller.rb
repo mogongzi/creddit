27a86bd89a07995fe7e1d291ebaf6eca8eaf9a6a
mogongzi <superrenyuan@hotmail.com>
2020-02-10 22:29:06 +0800

    import bootstrap and add community scaffold

---
+++ app/controllers/communities_controller.rb
@@ -0,0 +1,28 @@
+class CommunitiesController < ApplicationController
+  before_action :authenticate_account!, except: [:index, :show]
+
+  def index
+    @communities = Community.all
+  end
+
+  def show; end
+
+  def new
+    @community = Community.new community_values
+    @community.account_id = current_account.id
+
+    if @community.save
+      redirect_to communities_path
+    else
+      render :new
+    end
+  end
+
+  def create; end
+
+  private
+
+  def community_values
+    params.require(:community).permit(:name, :url, :rules)
+  end
+end
